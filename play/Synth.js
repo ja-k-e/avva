7f4b690c8222c02f5c1b2792b7c3575f58252879
jake <jake.albaugh@gmail.com>
2023-06-04 21:04:49 +0000

    saving

--- play/Synth.js
+++ play/Synth.js
@@ -1,9 +1,33 @@
 export class Synth {
   constructor() {
-    this.synths = [new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(),new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth()];
     const main = new Tone.Gain();
     main.toDestination();
     main.gain.value = 0.6;
+    this.setupChord(main);
+    this.setupNotes(main);
+  }
+
+  setupChord(main) {
+    this.chord = [new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth()];
+    this.chord.forEach((synth, i) => {
+      synth.set({
+        envelope: { attack: 0.3 },
+        oscillator: { type: "sine4" },
+        portamento: 1
+      });
+      const pan = new Tone.Panner();
+      pan.pan.value = ((i / this.chord.length) - 0.5) / 0.5;
+      const gain = new Tone.Gain();
+      gain.gain.value = 0.1;
+      synth.connect(pan);
+      pan.connect(gain);
+      gain.connect(main);
+    });
+    this.chord.forEach((synth) => synth.triggerAttack("C3"));
+  }
+
+  setupNotes(main) {
+    this.synths = [new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth(), new Tone.Synth()];
     this.synths.forEach((synth, i) => {
       synth.set({
         envelope: { attack: 0.2 },
@@ -21,12 +45,13 @@ export class Synth {
     this.synths.forEach((synth) => synth.triggerAttack("C3"));
   }
 
-  play({ chord }) {
-    this.synths.forEach((synth, i) => {
-      const { notation, octave } = chord.notes[i % chord.notes.length];
-      synth.setNote(`${notation}${octave + 3 + Math.floor(i % 3)}`);
+  playChord(chord) {
+    this.chord.forEach((synth, i) => {
+      const {notation, octave} = chord.notes[i % chord.notes.length];
+      synth.setNote(`${notation}${octave + i < 4 ? 2 : 5}`);
     });
   }
+
   playNotes(notes) {
     this.synths.forEach((synth, i) => {
       const note = notes[i % notes.length];
